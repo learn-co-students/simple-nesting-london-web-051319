89f74c4d6d2915b9a2d3688c69fb3cee02f9f6ec
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 10:08:37 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,13 +57,15 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
+#     :languages => ["LISP", "C"]
 # }
 
 	programmer_hash = 
@@ -79,7 +83,14 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:yukihiro_matsumoto] = 
+    {
+          :known_for => "Ruby", 
+          :languages => ["LISP", "C"]
+      
+    }
+    
+    return programmer_hash
     
 end
 
@@ -102,6 +113,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     
+     return programmer_hash
      
 end
 
@@ -121,5 +135,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     
+     return programmer_hash
 end
